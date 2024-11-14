9b10cf89267a9fc408cc49b74cc5980e67556aa1
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 00:40:18 +0100

    feat: Add WaitingRoomResponseDTO

---
+++ src/main/java/org/youcode/waitingroom/waitingRoom/application/dto/waitingRoom/WaitingRoomResponseDTO.java
@@ -0,0 +1,14 @@
+package org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom;
+
+import org.youcode.waitingroom.waitingRoom.domain.entity.enums.Algorithm;
+import org.youcode.waitingroom.waitingRoom.domain.entity.enums.TypeMode;
+
+import java.time.LocalDate;
+
+public record WaitingRoomResponseDTO(
+        LocalDate date,
+        int capacity,
+        Algorithm algorithm,
+        TypeMode mode
+) {
+}
