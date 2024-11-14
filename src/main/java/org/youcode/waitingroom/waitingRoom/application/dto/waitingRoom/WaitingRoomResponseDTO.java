7fa77557630af92695b27a03c565cd7cf5f73bd5
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 00:48:36 +0100

    feat: Update WaitingRoomResponseDTO class: add list of visits

--- src/main/java/org/youcode/waitingroom/waitingRoom/application/dto/waitingRoom/WaitingRoomResponseDTO.java
+++ src/main/java/org/youcode/waitingroom/waitingRoom/application/dto/waitingRoom/WaitingRoomResponseDTO.java
@@ -1,14 +1,17 @@
 package org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom;
 
+import org.youcode.waitingroom.visit.domain.Visit;
 import org.youcode.waitingroom.waitingRoom.domain.entity.enums.Algorithm;
 import org.youcode.waitingroom.waitingRoom.domain.entity.enums.TypeMode;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public record WaitingRoomResponseDTO(
         LocalDate date,
         int capacity,
         Algorithm algorithm,
-        TypeMode mode
+        TypeMode mode,
+        List<Visit> visits
 ) {
 }
