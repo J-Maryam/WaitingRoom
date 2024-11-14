7f511d721171909986762c897c8c062423d945fa
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 00:28:52 +0100

    feat: update VisitorRequestDTO: add @UniqueValue annotation to validate the unicity of the name

--- src/main/java/org/youcode/waitingroom/waitingRoom/application/dto/visitor/VisitorRequestDTO.java
+++ src/main/java/org/youcode/waitingroom/waitingRoom/application/dto/visitor/VisitorRequestDTO.java
@@ -1,8 +1,12 @@
 package org.youcode.waitingroom.waitingRoom.application.dto.visitor;
 
 import jakarta.validation.constraints.NotBlank;
+import org.youcode.waitingroom.common.application.validation.annotation.Exists;
+import org.youcode.waitingroom.common.application.validation.annotation.UniqueValue;
+import org.youcode.waitingroom.waitingRoom.domain.entity.Visitor;
 
 public record VisitorRequestDTO(
+        @UniqueValue(entityClass = Visitor.class, fieldName = "name", message = "Visitor already exists with this name")
         @NotBlank String name
 ) {
 }
