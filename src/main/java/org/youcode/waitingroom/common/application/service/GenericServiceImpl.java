2374d6a17c6aa7da5ee30d0297448caa99d1f2b1
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-14 09:38:05 +0100

    implement getAll method in GernericeSeriveImpl

---
+++ src/main/java/org/youcode/waitingroom/common/application/service/GenericServiceImpl.java
@@ -0,0 +1,57 @@
+package org.youcode.waitingroom.common.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
+import org.youcode.waitingroom.common.application.dto.PagedResponse;
+import org.youcode.waitingroom.common.application.mapper.GenericMapper;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Validated
+public class GenericServiceImpl<T, ID, RequestDto, ResponseDto> implements GenericService<T, ID, RequestDto, ResponseDto> {
+
+    protected JpaRepository<T, ID> repository;
+    protected GenericMapper<T, RequestDto, ResponseDto> mapper;
+
+    @Override
+    public PagedResponse<ResponseDto> getAll(Pageable pageable) {
+        Page<T> dtoPage = repository.findAll(pageable);
+        List<ResponseDto> dtoList = dtoPage.getContent().stream().map(mapper::toDto).toList();
+        return new PagedResponse<>(
+                dtoList,
+                dtoPage.getNumber(),
+                dtoPage.getSize(),
+                dtoPage.getTotalElements(),
+                dtoPage.getTotalPages(),
+                dtoPage.isLast()
+        );
+    }
+
+    @Override
+    public ResponseDto getById(ID id) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto create(RequestDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public ResponseDto update(ID id, RequestDto requestDto) {
+        return null;
+    }
+
+    @Override
+    public void delete(ID id) {
+
+    }
+}
