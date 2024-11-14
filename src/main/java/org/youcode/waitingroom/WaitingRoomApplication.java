69bd1b9699b8c6d458d4e78609ad859ef4366247
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 00:12:05 +0100

    feat: Add WrmConfigProperties to mapping the properties of the yml file configuration

--- src/main/java/org/youcode/waitingroom/WaitingRoomApplication.java
+++ src/main/java/org/youcode/waitingroom/WaitingRoomApplication.java
@@ -2,8 +2,11 @@ package org.youcode.waitingroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.youcode.waitingroom.config.WrmConfigProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(WrmConfigProperties.class)
 public class WaitingRoomApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/youcode/waitingroom/config/WrmConfigProperties.java
+++ src/main/java/org/youcode/waitingroom/config/WrmConfigProperties.java
@@ -1,4 +1,19 @@
 package org.youcode.waitingroom.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.youcode.waitingroom.waitingRoom.domain.entity.enums.Algorithm;
+import org.youcode.waitingroom.waitingRoom.domain.entity.enums.TypeMode;
+
+@Configuration
+@ConfigurationProperties(prefix = "wrm")
+@Getter
+@Setter
 public class WrmConfigProperties {
+    private TypeMode mode = TypeMode.FULL_TIME;
+    private Algorithm algorithm = Algorithm.FIFO;
+    private int maxPerDay = 15;
+
 }
