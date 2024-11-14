fdcd4a7c458d099cc98cab272d9e4d53bab43da7
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 00:37:23 +0100

    feat: Add WaitingRoomResponseDTO

---
+++ src/main/java/org/youcode/waitingroom/waitingRoom/application/dto/waitingRoom/WaitingRoomRequestDTO.java
@@ -0,0 +1,30 @@
+package org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.youcode.waitingroom.common.application.validation.annotation.UniqueValue;
+import org.youcode.waitingroom.waitingRoom.domain.entity.Visitor;
+import org.youcode.waitingroom.waitingRoom.domain.entity.enums.Algorithm;
+import org.youcode.waitingroom.waitingRoom.domain.entity.enums.TypeMode;
+
+import java.time.LocalDate;
+
+public record WaitingRoomRequestDTO(
+
+        @NotNull
+        @FutureOrPresent
+        LocalDate date,
+
+        @NotNull
+        int capacity,
+
+        @NotBlank
+        Algorithm algorithm,
+
+        @NotNull
+        TypeMode mode
+) {
+}
