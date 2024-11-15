c7b40c5779f2586b5279b3d1c9e5f3b19692000c
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 14:58:55 +0100

    feat: change VisitId to record

--- src/main/java/org/youcode/waitingroom/visit/domain/entity/VisitId.java
+++ src/main/java/org/youcode/waitingroom/visit/domain/entity/VisitId.java
@@ -1,4 +1,13 @@
 package org.youcode.waitingroom.visit.domain.entity;
 
-public record VisitId() {
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+
+@Embeddable
+public record VisitId(
+        @Column(name = "visitor_id") Long visitorId,
+        @Column(name = "waiting_list_id") Long waitingListId
+) implements Serializable {
 }
