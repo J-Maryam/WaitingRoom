38d475417eeb2ea7e3d2b6c4b0da85d8833250d7
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 09:37:21 +0100

    feat: delete unused imports

--- src/main/java/org/youcode/waitingroom/waitingRoom/application/service/Impl/WaitingRoomServiceImpl.java
+++ src/main/java/org/youcode/waitingroom/waitingRoom/application/service/Impl/WaitingRoomServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
-import org.youcode.waitingroom.common.application.service.GenericService;
 import org.youcode.waitingroom.common.application.service.GenericServiceImpl;
 import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomRequestDTO;
 import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomResponseDTO;
