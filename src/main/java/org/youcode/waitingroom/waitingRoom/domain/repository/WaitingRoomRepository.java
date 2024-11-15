df6d9366a32f4627b9eaab1c24a8d7edbdf61084
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 09:20:35 +0100

    add @Repository annotation in WaitingRoomRepository

--- src/main/java/org/youcode/waitingroom/waitingRoom/domain/repository/WaitingRoomRepository.java
+++ src/main/java/org/youcode/waitingroom/waitingRoom/domain/repository/WaitingRoomRepository.java
@@ -1,7 +1,9 @@
 package org.youcode.waitingroom.waitingRoom.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.youcode.waitingroom.waitingRoom.domain.entity.WaitingRoom;
 
+@Repository
 public interface WaitingRoomRepository extends JpaRepository<WaitingRoom, Long> {
 }
