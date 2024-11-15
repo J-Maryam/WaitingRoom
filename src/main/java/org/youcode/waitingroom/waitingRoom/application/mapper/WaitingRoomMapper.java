99b24e15aac5e4181936e98a9cbca7646cd18b4e
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 08:36:03 +0100

    feat: Update WaitingRoomMapper: add @Mapper annotation

--- src/main/java/org/youcode/waitingroom/waitingRoom/application/mapper/WaitingRoomMapper.java
+++ src/main/java/org/youcode/waitingroom/waitingRoom/application/mapper/WaitingRoomMapper.java
@@ -1,9 +1,11 @@
 package org.youcode.waitingroom.waitingRoom.application.mapper;
 
+import org.mapstruct.Mapper;
 import org.youcode.waitingroom.common.application.mapper.GenericMapper;
 import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomRequestDTO;
 import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomResponseDTO;
 import org.youcode.waitingroom.waitingRoom.domain.entity.WaitingRoom;
 
+@Mapper(componentModel = "spring")
 public interface WaitingRoomMapper extends GenericMapper<WaitingRoom, WaitingRoomRequestDTO, WaitingRoomResponseDTO> {
 }
