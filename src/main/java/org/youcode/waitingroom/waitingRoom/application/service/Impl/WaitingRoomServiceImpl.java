9079c2d230d9130e382279075a481b3e7e8f5821
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 22:41:18 +0100

    feat: Implement create method in WaitingRoomServiceImpl

--- src/main/java/org/youcode/waitingroom/waitingRoom/application/service/Impl/WaitingRoomServiceImpl.java
+++ src/main/java/org/youcode/waitingroom/waitingRoom/application/service/Impl/WaitingRoomServiceImpl.java
@@ -1,9 +1,11 @@
 package org.youcode.waitingroom.waitingRoom.application.service.Impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import org.youcode.waitingroom.common.application.service.GenericServiceImpl;
+import org.youcode.waitingroom.config.WrmConfigProperties;
 import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomRequestDTO;
 import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomResponseDTO;
 import org.youcode.waitingroom.waitingRoom.application.mapper.WaitingRoomMapper;
@@ -16,7 +18,26 @@ import org.youcode.waitingroom.waitingRoom.domain.repository.WaitingRoomReposito
 @Validated
 public class WaitingRoomServiceImpl extends GenericServiceImpl<WaitingRoom, Long, WaitingRoomRequestDTO, WaitingRoomResponseDTO> implements WaitingRoomService {
 
-    public WaitingRoomServiceImpl(WaitingRoomRepository repository, WaitingRoomMapper mapper) {
+    private final WrmConfigProperties wrmConfigProperties;
+    private final WaitingRoomRepository repository;
+    private final WaitingRoomMapper mapper;
+
+    public WaitingRoomServiceImpl(WaitingRoomRepository repository, WaitingRoomMapper mapper, WrmConfigProperties wrmConfigProperties) {
         super(repository, mapper);
+        this.wrmConfigProperties = wrmConfigProperties;
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public WaitingRoomResponseDTO create(WaitingRoomRequestDTO requestDto) {
+        WaitingRoom waitingRoom = mapper.toEntity(requestDto);
+
+        waitingRoom.setMode(wrmConfigProperties.getMode());
+        waitingRoom.setAlgorithm(wrmConfigProperties.getAlgorithm());
+        waitingRoom.setCapacity(wrmConfigProperties.getMaxPerDay());
+
+        WaitingRoom savedEntity = repository.save(waitingRoom);
+        return mapper.toDto(savedEntity);
     }
 }
