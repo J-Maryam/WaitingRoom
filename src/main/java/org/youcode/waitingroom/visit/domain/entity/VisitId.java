770a05d3aa4bdec2a354988dad174594565cff56
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 14:53:47 +0100

    update port in yml configurations

--- src/main/java/org/youcode/waitingroom/visit/domain/entity/VisitId.java
+++ src/main/java/org/youcode/waitingroom/visit/domain/entity/VisitId.java
@@ -1,17 +1,4 @@
 package org.youcode.waitingroom.visit.domain.entity;
 
-import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Embeddable
-public class VisitId implements Serializable {
-    private Long visitorId;
-    private Long waitingRoomId;
+public record VisitId() {
 }
