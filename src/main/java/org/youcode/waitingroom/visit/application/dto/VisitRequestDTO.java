8e5982ee6ae677a933aebfd338e14a318cfc5052
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 01:01:38 +0100

    feat: update VisitRequestDTO

--- src/main/java/org/youcode/waitingroom/visit/application/dto/VisitRequestDTO.java
+++ src/main/java/org/youcode/waitingroom/visit/application/dto/VisitRequestDTO.java
@@ -3,9 +3,12 @@ package org.youcode.waitingroom.visit.application.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
+import org.youcode.waitingroom.common.application.validation.annotation.Exists;
 import org.youcode.waitingroom.visit.domain.enums.Status;
 import org.youcode.waitingroom.waitingRoom.application.dto.visitor.VisitorResponseDTO;
 import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomResponseDTO;
+import org.youcode.waitingroom.waitingRoom.domain.entity.Visitor;
+import org.youcode.waitingroom.waitingRoom.domain.entity.WaitingRoom;
 
 import java.time.LocalDateTime;
 
@@ -28,9 +31,11 @@ public record VisitRequestDTO(
         LocalDateTime endTime,
 
         @NotNull
+        @Exists(entityClass = Visitor.class, message = "Visitor Id does not exists")
         VisitorResponseDTO visitor,
 
         @NotNull
+        @Exists(entityClass = WaitingRoom.class, message = "Waiting Room Id does not exists")
         WaitingRoomResponseDTO waitingRoom
 ) {
 }
