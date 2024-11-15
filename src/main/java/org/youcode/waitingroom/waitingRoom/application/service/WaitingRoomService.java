6b0318601b9d3776a84315ad44b2544cb2e44c74
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-15 09:29:52 +0100

    feat: add WaitingRoomService interface

---
+++ src/main/java/org/youcode/waitingroom/waitingRoom/application/service/WaitingRoomService.java
@@ -0,0 +1,10 @@
+package org.youcode.waitingroom.waitingRoom.application.service;
+
+import org.youcode.waitingroom.common.application.service.GenericService;
+import org.youcode.waitingroom.common.application.service.GenericServiceImpl;
+import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomRequestDTO;
+import org.youcode.waitingroom.waitingRoom.application.dto.waitingRoom.WaitingRoomResponseDTO;
+import org.youcode.waitingroom.waitingRoom.domain.entity.WaitingRoom;
+
+public interface WaitingRoomService extends GenericService<WaitingRoom, Long, WaitingRoomRequestDTO, WaitingRoomResponseDTO> {
+}
