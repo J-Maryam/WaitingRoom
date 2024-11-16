7253f51f0dca81bd4f2ac1c46906c8e301d817b8
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-16 12:46:10 +0100

    feat: Update VisitRepository: change type of id from Long to VisitId

--- src/main/java/org/youcode/waitingroom/visit/domain/repository/VisitRepository.java
+++ src/main/java/org/youcode/waitingroom/visit/domain/repository/VisitRepository.java
@@ -3,7 +3,8 @@ package org.youcode.waitingroom.visit.domain.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.youcode.waitingroom.visit.domain.entity.Visit;
+import org.youcode.waitingroom.visit.domain.entity.VisitId;
 
 @Repository
-public interface VisitRepository extends JpaRepository<Visit, Long> {
+public interface VisitRepository extends JpaRepository<Visit, VisitId> {
 }
