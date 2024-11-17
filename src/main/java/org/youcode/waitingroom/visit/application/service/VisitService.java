79b4ca84614ab584b5dff73b39a6960a0cb72bbe
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-11-17 15:56:33 +0100

    feat: add getAllVisitorsForWaitingRoom method in VisitService

--- src/main/java/org/youcode/waitingroom/visit/application/service/VisitService.java
+++ src/main/java/org/youcode/waitingroom/visit/application/service/VisitService.java
@@ -1,5 +1,7 @@
 package org.youcode.waitingroom.visit.application.service;
 
+import org.springframework.data.domain.Pageable;
+import org.youcode.waitingroom.common.application.dto.PagedResponse;
 import org.youcode.waitingroom.common.application.service.GenericService;
 import org.youcode.waitingroom.visit.application.dto.VisitRequestDTO;
 import org.youcode.waitingroom.visit.application.dto.VisitResponseDTO;
@@ -7,4 +9,5 @@ import org.youcode.waitingroom.visit.domain.entity.Visit;
 import org.youcode.waitingroom.visit.domain.entity.VisitId;
 
 public interface VisitService extends GenericService<Visit, VisitId, VisitRequestDTO, VisitResponseDTO> {
+    PagedResponse<VisitResponseDTO> getAllVisitorsForWaitingRoom(Long waitingRoomId, Pageable pageable);
 }
